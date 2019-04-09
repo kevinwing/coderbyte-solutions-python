6fd5adc28fa5faaafda71cf4d905d20367fbcfb1
Kevin Wing <kevinrwing@gmail.com>
2019-04-09 15:27:06 -0700

    edited instructions

--- hard/kaprekars_constant.py
+++ hard/kaprekars_constant.py
@@ -9,8 +9,10 @@ number), and subtract the smaller number from the bigger number. Then repeat
 the previous step. Performing this routine will always cause you to reach a
 fixed number: 6174. Then performing the routine on 6174 will always give you
 6174 (7641 - 1467 = 6174). Your program should return the number of times this
-routine must be performed until 6174 is reached. For example: if num is 3524
-your program should return 3 because of the following steps:
+routine must be performed until 6174 is reached.
+For example: if num is 3524 your program should return 3 because of the
+following steps:
+
 (1) 5432 - 2345 = 3087,
 (2) 8730 - 0378 = 8352,
 (3) 8532 - 2358 = 6174.
